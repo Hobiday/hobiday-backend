8a81579e33b26cf4625d1ed6dcb6c871e3cb9c1e
CH <chyun51@naver.com>
2024-11-30 11:31:01 +0900

    ApiResponse 응답방식 성공시 예외처리 통일 적용

--- src/main/java/com/example/hobiday_backend/global/s3/controller/FileController.java
+++ src/main/java/com/example/hobiday_backend/global/s3/controller/FileController.java
@@ -1,15 +1,12 @@
 package com.example.hobiday_backend.global.s3.controller;
 
-import com.example.hobiday_backend.global.dto.SuccessRes;
+import com.example.hobiday_backend.global.dto.ApiResponse;
 import com.example.hobiday_backend.global.dto.file.PreSignedUrlRequest;
 import com.example.hobiday_backend.global.dto.file.PresignedUrlResponse;
 import com.example.hobiday_backend.global.s3.service.FileService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,7 +18,7 @@ public class FileController {
     @Operation(summary = "Presigned URL 요청", description = "파일 업로드를 위한 presigned URL을 생성하는 API" +
             "\n버킷명/prefix/fileName 위치에 파일 생성")
     @PostMapping
-    public ResponseEntity<PresignedUrlResponse> uploadPhoto(@RequestBody PreSignedUrlRequest presignedUrlRequest) {
+    public ApiResponse<PresignedUrlResponse> uploadPhoto(@RequestBody PreSignedUrlRequest presignedUrlRequest) {
         // Presigned URL 생성 요청 처리
         PresignedUrlResponse response = fileService.getUploadPresignedUrl(
                 presignedUrlRequest.getPrefix(),
@@ -29,7 +26,8 @@ public class FileController {
         );
 
         // 성공적인 응답을 반환
-        return ResponseEntity.ok(response);
+//        return ResponseEntity.ok(response);
+        return ApiResponse.success(response);
     }
 }
 
