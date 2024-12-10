24735b8766ef41ff4737f4ee70193ca8e21134d7
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2024-12-10 19:16:44 +0900

    nginx

--- src/main/java/com/example/hobiday_backend/HobidayApplication.java
+++ src/main/java/com/example/hobiday_backend/HobidayApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class HobidayApplication {
+public class HobidayApplication { //323423
 
     public static void main(String[] args) {
         SpringApplication.run(HobidayApplication.class, args);
